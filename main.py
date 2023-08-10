30b350af977e17797581ae055d46df71b23749a8
Dillon Durrant <dillondurrant2@gmail.com>
2023-08-10 12:03:01 -0400

    Included JSON

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ Build from output folder: pyinstaller ..\sofia_test\main.py -n sofia-0.3.5.exe -
 """
 from gui import GUI 
 import json
+import os
 
 
 
 if __name__ == "__main__":
-    with open('C:\\USers\\ddurrant\\OneDrive - Fortress Information Security\\Documents\\Python Scripts\\SOFIA\\testjson.json') as json_file:
+    with open(os.path.join(os.path.dirname(__file__), "commands.json")) as json_file: #'C:\\USers\\ddurrant\\OneDrive - Fortress Information Security\\Documents\\Python Scripts\\SOFIA\\testjson.json') as json_file:
         data = json.load(json_file)
         newgui = GUI(data)
